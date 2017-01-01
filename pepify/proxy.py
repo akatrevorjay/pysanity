0616dff6467525f4e9d33c8758cf0181bbfa4fdb
Trevor Joynson (trevorj) <github@skywww.net>
2017-01-01 03:06:14 -0800

    Up

--- pepify/proxy.py
+++ pepify/proxy.py
@@ -1,7 +1,39 @@
 import wrapt
-
-from .adaptation import CachingAdaptation
+import weakref
+from .util import _sentinel, lazyproperty
 from .adapters import camelize
+
+
+class Adaptation(object):
+
+    def __init__(self, read):
+        self.read = read
+
+    def get_attr(self, name, wrapped):
+        for adapted_name in self.read(name):
+            try:
+                return getattr(wrapped, adapted_name)
+            except AttributeError:
+                # sugar
+                continue
+        raise AttributeError(name)
+
+
+class CachingAdaptation(Adaptation):
+
+    @lazyproperty
+    def cache(self):
+        return weakref.WeakValueDictionary()
+
+    def get_attr(self, name, wrapped):
+        try:
+            return self.cache[name]
+        except KeyError:
+            # pass here to not chain exceptions
+            pass
+        val = super(CachingAdaptation, self).get_attr(name, wrapped)
+        self.cache[name] = val
+        return val
 
 
 class PepifyProxy(wrapt.ObjectProxy):
@@ -14,4 +46,6 @@ class PepifyProxy(wrapt.ObjectProxy):
         try:
             return super(PepifyProxy, self).__getattr__(name)
         except AttributeError:
-            return self.__adapter.get_attr(name, wrapped=self.__wrapped__)
+            # pass here to not chain exceptions
+            pass
+        return self.__adapter.get_attr(name, wrapped=self.__wrapped__)

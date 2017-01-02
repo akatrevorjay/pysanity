f275acdf3833c91923cee63b50213cd539a7a2cf
Trevor Joynson (trevorj) <github@skywww.net>
2017-01-01 22:41:02 -0800

    Specify reqs for adapt

--- pysanity/proxy.py
+++ pysanity/proxy.py
@@ -8,23 +8,31 @@ from . import adapters
 
 class Adapter(object):
 
-    def __init__(self, get, predicate=None):
+    def __init__(self, get, adapted_predicate=None, should_adapt=None):
         self.get = get
-        self.predicate = predicate
+        self.adapted_predicate = adapted_predicate
+        self.should_adapt = should_adapt
 
-    def find_attr(self, name, wrapped, predicate=None):
-        if not predicate:
-            predicate = self.predicate
+    def find_attr(self, name, wrapped):
+
+        def _fail():
+            raise AttributeError(name)
+
+        if self.should_adapt and not self.should_adapt(name):
+            _fail()
+
+        adapted_predicate = self.adapted_predicate
 
         for adapted_name in self.get(name):
             try:
                 obj = getattr(wrapped, adapted_name)
             except AttributeError:
                 continue
-            if not predicate or predicate(name, adapted_name, obj):
+
+            if not adapted_predicate or adapted_predicate(name, adapted_name, obj):
                 return obj
 
-        raise AttributeError(name)
+        _fail()
 
 
 class CachedAdapter(Adapter):
@@ -33,17 +41,18 @@ class CachedAdapter(Adapter):
     def cache(self):
         return weakref.WeakValueDictionary()
 
-    def find_attr(self, name, wrapped, predicate=None):
+    def find_attr(self, name, wrapped):
         try:
             return self.cache[name]
         except KeyError:
-            val = super(CachedAdapter, self).find_attr(name, wrapped, predicate)
+            obj = super(CachedAdapter, self).find_attr(name, wrapped)
 
-            self.cache[name] = val
-            return val
+            self.cache[name] = obj
+            return obj
 
 
 class PepifyProxy(wrapt.ObjectProxy):
+
     def __init__(self, wrapped, adapter):
         super(PepifyProxy, self).__init__(wrapped)
         self._self_adapter = adapter
@@ -75,22 +84,29 @@ class RecursivePepifyProxy(PepifyProxy):
         self._self_should_recurse = should_recurse
 
     def __getattr__(self, name):
-        val = super(RecursivePepifyProxy, self).__getattr__(name)
+        obj = super(RecursivePepifyProxy, self).__getattr__(name)
 
         for check in self._self_should_recurse:
-            if check(val):
-                return RecursivePepifyProxy(val, self._self_adapter, should_recurse=self._self_should_recurse)
+            if check(obj):
+                return RecursivePepifyProxy(obj, self._self_adapter, should_recurse=self._self_should_recurse)
 
-        return val
+        return obj
 
 
-def obj_is_function_or_method(name, adapted_name, obj):
+def adapted_obj_is_function_or_method(name, adapted_name, obj):
     return inspect.isfunction(obj) or inspect.ismethod(obj)
 
 
-def make_proxy(wrapped, get=adapters.lower_camel_case, predicate=obj_is_function_or_method, cache=True, recurse=True):
+def make_proxy(
+    wrapped,
+    get=adapters.lower_camel_case,
+    should_adapt=adapters.is_snake_case,
+    adapted_predicate=adapted_obj_is_function_or_method,
+    cache=True,
+    recurse=True,
+):
     adapter_factory = cache and CachedAdapter or Adapter
     proxy_factory = recurse and RecursivePepifyProxy or PepifyProxy
 
-    adapter = adapter_factory(get=get, predicate=predicate)
+    adapter = adapter_factory(get=get, adapted_predicate=adapted_predicate, should_adapt=should_adapt)
     return proxy_factory(wrapped, adapter)
